2e68987f959382cf41766b8df5474d7888cb8217
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-07-26 16:37:22 +0700

    update

--- scripts/generate_annotations/AnnotationsGenerator.py
+++ scripts/generate_annotations/AnnotationsGenerator.py
@@ -69,7 +69,7 @@ class AnnotationsGenerator:
         if save_path:
             save_path = save_path + "/" + self.name + "_junctions.txt"
             print(f"==>> Saving {save_path}")
-            self._encode_box(junctions, save_path)
+            self._encode_box(junctions, save_path, mode="junctions")
             
     def draw_grains(self, save_path=None, show=False):
         img_copy = self.img.copy()
@@ -118,8 +118,74 @@ class AnnotationsGenerator:
             print(f"==>> Saving {save_path}")
             cv2.imwrite(save_path, img_copy)
     
-    def encode_grains(self):
-        pass
+    def encode_grains(self, save_path=None):
+        """
+        Encode the grain bounding boxes as (class_label, x, y, w, h), all relative to the whole image
+        """
+        img_copy = self.img.copy()
+        grains = list()
+        
+        for edge in self.edges:
+            x1, y1, x2, y2 = edge
+            
+            if (x2, y2) in self.junctions.return_terminal():
+                
+                # ============================================== #
+                #     Conditions to avoid small bounding boxes   #
+                # ============================================== #
+                
+                if abs(y1 - y2) <= 10:
+                    if y1 < y2:
+                        cv2.rectangle(img_copy, pt1=(x1, y1-25), pt2=(x2, y2+25), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1, y1-25, x2, y2+25)
+                        xywh = self._xyxy2xywh(xyxy)
+                    elif y1 >= y2:
+                        cv2.rectangle(img_copy, pt1=(x1, y1+25), pt2=(x2, y2-25), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1, y1+25, x2, y2-25)
+                        xywh = self._xyxy2xywh(xyxy)
+                        
+                elif abs(y1 - y2) < 25:
+                    if y1 < y2:
+                        cv2.rectangle(img_copy, pt1=(x1, y1-10), pt2=(x2, y2+10), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1, y1-10, x2, y2+10)
+                        xywh = self._xyxy2xywh(xyxy)
+                    elif y1 >= y2:
+                        cv2.rectangle(img_copy, pt1=(x1, y1+10), pt2=(x2, y2-10), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1, y1+10, x2, y2-10)
+                        xywh = self._xyxy2xywh(xyxy)
+                        
+                elif abs(x1 - x2) <= 10:
+                    if x1 < x2:
+                        cv2.rectangle(img_copy, pt1=(x1-25, y1), pt2=(x2+25, y2), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1-25, y1, x2+25, y2)
+                        xywh = self._xyxy2xywh(xyxy)
+                    elif x1 >= x2:
+                        cv2.rectangle(img_copy, pt1=(x1+25, y1), pt2=(x2-25, y2), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1+25, y1, x2-25, y2)
+                        xywh = self._xyxy2xywh(xyxy)
+                        
+                elif abs(x1 - x2) < 25:
+                    if x1 < x2:
+                        cv2.rectangle(img_copy, pt1=(x1-10, y1), pt2=(x2+10, y2), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1-10, y1, x2+10, y2)
+                        xywh = self._xyxy2xywh(xyxy)
+                    elif x1 >= x2:
+                        cv2.rectangle(img_copy, pt1=(x1+10, y1), pt2=(x2-10, y2), color=(0, 255, 255), thickness=2)
+                        xyxy = (x1+10, y1, x2-10, y2)
+                        xywh = self._xyxy2xywh(xyxy)
+                        
+                else:
+                    cv2.rectangle(img_copy, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 255), thickness=2)
+                    xyxy = (x1, y1, x2, y2)
+                    xywh = self._xyxy2xywh(xyxy)
+                    
+                grains.append(xywh)
+        
+        print(len(grains))
+        if save_path:
+            save_path = save_path + "/" + self.name + "_grains.txt"
+            print(f"==>> Saving {save_path}")
+            self._encode_box(grains, save_path, mode="grains")
     
     def draw_junctions_grains(self):
         pass
@@ -140,29 +206,67 @@ class AnnotationsGenerator:
         plt.tight_layout()
         plt.show()
     
-    def _encode_box(self, boxes, save_path) -> None:
+    def _encode_box(self, boxes, save_path, mode) -> None:
         """
         Args:
             boxes (list): list of all boxes
             save_path (str): file path
+            mode (str): "junctions" or "grains"
         """
+        assert mode in ["junctions", "grains"], "Invalid mode"
+        
         height, width, _ = self.img.shape
         
-        with open(save_path, "w") as f:
-            for x, y in boxes:
-                x, y = int(x) / width, int(y) / height
-                w, h = 26 / width, 26 / height
-                class_label = 0
-                f.write(f"{class_label} {x} {y} {w} {h}\n")
+        if mode == "junctions":
+            with open(save_path, "w") as f:
+                for x, y in boxes:
+                    x, y = int(x) / width, int(y) / height
+                    w, h = 26 / width, 26 / height
+                    class_label = 0
+                    f.write(f"{class_label} {x} {y} {w} {h}\n")
+                    
+        elif mode == "grains":
+            pass
+                
+    def _xyxy2xywh(self, xyxy) -> tuple:
+        """
+        Turns a bounding box in (x1, y1, x2, y2) format into (x, y, w, h)
+
+        Args:
+            xyxy (tuple): (x1, y1, x2, y2)
+            
+        Returns:
+            tuple: (x, y, w, h)
+        """
+        x1, y1, x2, y2 = xyxy
+        
+        # Compute the minimum and maximum coordinates
+        x_min = min(x1, x2)
+        y_min = min(y1, y2)
+        x_max = max(x1, x2)
+        y_max = max(y1, y2)
+        
+        # Compute width and height
+        width = x_max - x_min
+        height = y_max - y_min
+        
+        # Compute center coordinates
+        x_center = x_min + width / 2
+        y_center = y_min + height / 2
+    
+        xywh = (x_center, y_center, width, height)
+        
+        return xywh
 
 def test():
     img_path = "data/raw/Asian/10_2_1_1_1_DSC01291.jpg"
     ricepr_path = "data/raw/Asian/10_2_1_1_1_DSC01291.ricepr"
     annotations_generator = AnnotationsGenerator(img_path=img_path, ricepr_path=ricepr_path)
-    annotations_generator.draw_junctions(show=True, remove_end_generating=True)
-    annotations_generator.draw_grains(show=True)
-    annotations_generator.encode_junctions(save_path=".", remove_end_generating=True)
-    os.remove("10_2_1_1_1_DSC01291_junctions.txt")
+    # annotations_generator.draw_junctions(show=True, remove_end_generating=True)
+    # annotations_generator.draw_grains(show=True)
+    # annotations_generator.encode_junctions(save_path=".", remove_end_generating=True)
+    # os.remove("10_2_1_1_1_DSC01291_junctions.txt")
+    annotations_generator.encode_grains(save_path=".")
     print("All tests passed")
     
 
