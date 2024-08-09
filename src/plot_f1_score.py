37c5000db23c8a968b4ddcb469da8671e5889ea7
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-08-09 07:25:44 +0700

    update

--- src/plot_f1_score.py
+++ src/plot_f1_score.py
@@ -8,44 +8,45 @@ def plot_f1_score(xlsx_file, save_path):
     df = pd.read_excel(xlsx_file)
     df.columns = df.columns.str.strip()
     
-    columns = ['f1', 'precision', 'recall', 'abs(pr-rc)']
+    columns = ['f1', 'precision', 'recall']
     
     metrics = df[columns]
     
     # Plot
-    _, axes = plt.subplots(1, 4, figsize=(16, 6))
-    ax1, ax2, ax3, ax4 = axes
+    _, axes = plt.subplots(1, 3, figsize=(15, 6))
+    ax1, ax2, ax3 = axes
     
     # Plot for 'f1'
-    sns.histplot(metrics['f1'], kde=True, ax=ax1, color='blue', bins=30)
     mean_f1 = metrics['f1'].mean()
     ax1.axvline(mean_f1, color='blue', linestyle='dashed', linewidth=2, label=f'Mean: {mean_f1 * 100:.2f}%')
+    
+    factor = 1.0  # Change this if needed
+    threshold = metrics['f1'].mean() - factor * metrics['f1'].std()
+    count = metrics[metrics['f1'] <= threshold]['f1'].count()
+    
+    # Plot values below threshold
+    sns.histplot(metrics[metrics['f1'] <= threshold]['f1'], ax=ax1, color='blue', bins=10, label=f"Error analysis ({count})", alpha=0.2)
+    # Plot values above threshold
+    sns.histplot(metrics[metrics['f1'] > threshold]['f1'], kde=True, ax=ax1, color='blue', bins=20)
+    # Plot threshold
+    ax1.axvline(threshold, color='black', linestyle='dashed', linewidth=2, label=f'Threshold: {threshold:.4f}\n({factor} std below mean)')
+    
+    ax1.set_xlabel("$F_1$ Score")
     ax1.legend()
     
     # Plot for 'precision'
     sns.histplot(metrics['precision'], kde=True, ax=ax2, color='orange', bins=30)
+    ax2.set_xlabel("Precision")
     mean_precision = metrics['precision'].mean()
     ax2.axvline(mean_precision, color='orange', linestyle='dashed', linewidth=2, label=f'Mean: {mean_precision * 100:.2f}%')
     ax2.legend()
     
     # Plot for 'recall'
     sns.histplot(metrics['recall'], kde=True, ax=ax3, color='green', bins=30)
+    ax3.set_xlabel("Recall")
     mean_recall = metrics['recall'].mean()
     ax3.axvline(mean_recall, color='green', linestyle='dashed', linewidth=2, label=f'Mean: {mean_recall * 100:.2f}%')
     ax3.legend()
-    
-    # Plot for 'abs(pr-rc)' with threshold
-    factor = 2.0  # Change this if needed
-    threshold = metrics['abs(pr-rc)'].mean() + factor * metrics['abs(pr-rc)'].std()
-
-    # Plot values below threshold
-    sns.histplot(metrics[metrics['abs(pr-rc)'] <= threshold]['abs(pr-rc)'], kde=True, ax=ax4, color='red', bins=20, alpha=0.1)
-    
-    # Plot values above threshold
-    sns.histplot(metrics[metrics['abs(pr-rc)'] > threshold]['abs(pr-rc)'], kde=True, ax=ax4, color='red', bins=10, label="Error analysis")
-
-    ax4.axvline(threshold, color='black', linestyle='dashed', linewidth=2, label=f'Threshold: {threshold:.4f}')
-    ax4.legend()
     
     plt.tight_layout()
     plt.savefig(save_path)
