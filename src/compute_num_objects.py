2b4e649d0ca5b49f02a49bd31de3bd07bd884043
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-08-09 10:32:56 +0700

    add

---
+++ src/compute_num_objects.py
@@ -0,0 +1,31 @@
+import os
+
+
+def compute_num_objects(split_path):
+    """Compute the number of objects in a dataset"""
+    train_path = split_path + "/train/labels"
+    val_path = split_path + "/val/labels"
+    counter = 0
+    
+    for filename in os.listdir(train_path):
+        file_path = train_path + "/" + filename
+        with open(file_path) as f:
+            counter += len(f.readlines())
+        
+    for filename in os.listdir(val_path):
+        file_path = val_path + "/" + filename
+        with open(file_path) as f:
+            counter += len(f.readlines())
+    
+    num_obj = counter
+    obj_aver = counter / (len(os.listdir(train_path)) + len(os.listdir(val_path)))
+    
+    return num_obj, obj_aver
+
+
+if __name__ == "__main__":
+    split_path = "data/splits/split2"
+    num_obj, obj_aver = compute_num_objects(split_path)
+    print(f"==>> num_obj: {num_obj}")
+    print(f"==>> obj_aver: {obj_aver:.2f}")
+    
