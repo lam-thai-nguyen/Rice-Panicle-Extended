7fb0e60a8071cd94e3f78fb1de23aa1b403a6bf2
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-08-24 13:09:46 +0700

    update

--- src/assess_result.py
+++ src/assess_result.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts.compute_metrics.F1score import F1score
+import yaml
 
 
 def main(split_name, run_name, mode, conf, iou_threshold):
@@ -16,10 +17,15 @@ def main(split_name, run_name, mode, conf, iou_threshold):
 
 
 if __name__ == "__main__":
-    split_name = "split8"
+    with open("src/config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+        
+    confidence = config["confidence"]
+    
+    split_name = "split11"
     run_name = ""
-    mode = "train"
-    conf = 0.233
+    mode = "val"
+    conf = confidence[split_name][0] if mode == "train" else confidence[split_name][1]
     iou_threshold = 0.1
 
     main(split_name, run_name, mode, conf, iou_threshold)
--- src/visualize_result.py
+++ src/visualize_result.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts.visualize_predictions.Visualizer import Visualizer
+import yaml
 
 
 def main(split_name, run_name, plot_loss=False, predict_show=False, mode="train", conf=None, img_name=None, visual_mode="side", show=True, save_path=None):
@@ -25,31 +26,19 @@ def main(split_name, run_name, plot_loss=False, predict_show=False, mode="train"
     
 
 if __name__ == "__main__":
-    # ============================================= #
-    #                   HINTS                       #
-    # ============================================= #
-    conf_helpers = {  
-        "split4": [0.304, 0.309],
-        "split5": [0.244, 0.246],
-        "split6": [0.275, 0.270],  # train, val confs
-        "split7": [0.323, 0.322],
-        "split8": [0.294, 0.292],
-        "split9": [0.233, 0.233],
-        "split10": [0.310, 0.310],  
-        "split11": [],  
-    }
-    img_helpers = {
-        "lots_of_problems": "43_2_1_1_2_DSC09658_.jpg",
-        "lots_of_junctions": "37_2_1_1_1_DSC01221.jpg",
-    }
+    with open("src/config.yaml", 'r') as f:
+        config = yaml.safe_load(f)
+        
+    confidence = config["confidence"]
+    benchmark_image = config["benchmark_image"]
     
-    split_name = "split5"
+    split_name = "split9"
     run_name = ""
     plot_loss = False
     predict_show = True
     mode = "train"
-    conf = conf_helpers[split_name][0] if mode == "train" else conf_helpers[split_name][1]
-    img_name = img_helpers["lots_of_problems"]
+    conf = confidence[split_name][0] if mode == "train" else confidence[split_name][1]
+    img_name = benchmark_image["lots_of_problems"]
     visual_mode = "side"
     show = True
     save_path = None
