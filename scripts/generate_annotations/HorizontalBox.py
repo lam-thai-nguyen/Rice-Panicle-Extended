316e508b0c5fbde5f1a9ecdbb1006fd90470381a
Lam Thai Nguyen <thainguyen2893@gmail.com>
2025-01-13 06:15:18 +0700

    add new features for branches

--- scripts/generate_annotations/HorizontalBox.py
+++ scripts/generate_annotations/HorizontalBox.py
@@ -1,13 +1,22 @@
 class HorizontalBox:
     """hbb manager for an image"""
-    def __init__(self, junctions: list) -> None:
-        self.junctions = junctions
-        self.rects = list()
 
-    def run(self, width, height) -> list:
+    def __init__(self, junctions: list = None, branches: list = None) -> None:
+        self.junctions = junctions
+        self.branches = branches
+        self.rects_junctions = list()
+        self.rects_branches = list()
+
+    def run_junctions(self, width, height) -> list:
         for x, y in self.junctions:
             pt1 = (x - width // 2, y - height // 2)
             pt2 = (x + width // 2, y + height // 2)
-            self.rects.append(tuple([pt1, pt2]))
-        return self.rects
-    
+            self.rects_junctions.append(tuple([pt1, pt2]))
+        return self.rects_junctions
+
+    def run_branches(self) -> list:
+        for x1, y1, x2, y2 in self.branches:
+            # pt1 = (x1, y1): end point         |   secondary junction  | junction
+            # pt2 = (x2, y2): primary junction  |   end point           | end point
+            self.rects_branches.append(tuple([x1, y1, x2, y2]))
+        return self.rects_branches
